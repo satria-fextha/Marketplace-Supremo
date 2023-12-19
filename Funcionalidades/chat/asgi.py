c61ca61a305e6ad5de0ebf5c73cb4f7660fdd860
Jorge <jorgebuenobenete@gmail.com>
2023-12-19 13:31:53 +0100

    tu mensaje de commit aquí

--- Funcionalidades/chat/asgi.py
+++ Funcionalidades/chat/asgi.py
@@ -1,34 +1,16 @@
-Explicación:
+"""
+ASGI config for chat application.
 
-Este archivo de Python define la configuración de ASGI para una aplicación de chat Django Channels. ASGI es un estándar para desarrollar aplicaciones de red asincrónicas y reactivas.
+It exposes the ASGI callable as a module-level variable named `application`.
+"""
 
-Contenido:
-
-El archivo importa los siguientes módulos de Django Channels:
-
-ProtocolTypeRouter: Este módulo proporciona un router de protocolos que se utiliza para mapear protocolos a routers de URL.
-URLRouter: Este módulo proporciona un router de URL que se utiliza para mapear URL a consumidores.
-run_server: Este módulo proporciona una función que se utiliza para ejecutar un servidor ASGI.
-El archivo luego define una variable llamada application que es un router de protocolos. El router de protocolos se configura para mapear el protocolo WebSocket al router de URL ChatConsumer. Finalmente, el archivo llama a la función run_server para ejecutar un servidor ASGI que utiliza el router de protocolos application.
-
-Uso:
-
-Este archivo se utiliza para configurar y ejecutar un servidor ASGI para una aplicación de chat Django Channels. El servidor escucha solicitudes WebSocket y las envía al consumidor ChatConsumer. El consumidor maneja las solicitudes WebSocket y publica mensajes en una cola de mensajes. Otros clientes de chat que están conectados al servidor pueden entonces recibir los mensajes y actualizar sus listas de mensajes.
-
-Ruta:
-
-El archivo de Python se encuentra en la siguiente ruta:
-
-Funcionalidades/chat/asgi.py
-Esta ruta indica que el archivo se encuentra en la carpeta Funcionalidades/chat dentro del proyecto.
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+
+from chat.consumers import ChatConsumer
 
 application = ProtocolTypeRouter({
     "websocket": URLRouter([
         path("", ChatConsumer.as_asgi()),
     ])
 })
-
-from channels.asgi import run_server
-
-run_server(application)
